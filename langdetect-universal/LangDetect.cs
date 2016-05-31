f328d3eb317e7b00d5217af533d5e07a76111a9e
Blair Urish <burace17@gmail.com>
2016-05-30 22:19:07 -0500

    Initial commit

---
+++ langdetect-universal/LangDetect.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+namespace langdetect_universal
+{
+    public class Language
+    {
+        public Language(string name)
+        {
+            Name = name;
+            Occurances = 0;
+        }
+        public string Name { get; }
+        public int Occurances { get; set; }
+    }
+    public class WordStorage
+    {
+        private Dictionary<string, List<Language>> words = new Dictionary<string, List<Language>>();
+
+        public void AddWord(string word, Language l)
+        {
+            List<Language> languageList;
+            if (words.TryGetValue(word, out languageList))
+            {
+                // word is already in the dictionary
+                languageList.Add(l);
+            }
+            else
+            {
+                languageList = new List<Language>();
+                languageList.Add(l);
+                words.Add(word, languageList);
+            }
+        }   
+        
+        /// <summary>
+        /// Tries to find the given word in the dictionary. 
+        /// </summary>
+        /// <param name="word">The word to search for</param>
+        /// <returns>The list of languages the word is found in, or null if the word is not in the dictionary</returns>
+        public List<Language> FindWord(string word)
+        {
+            List<Language> languageList;
+            if (words.TryGetValue(word, out languageList))
+            {
+                return languageList;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class LangDetect
+    {
+        private WordStorage words;
+        private List<Language> languages = new List<Language>();
+        public LangDetect(WordStorage w)
+        {
+            words = w;
+        }
+
+        /// <summary>
+        /// Adds a language to the detector
+        /// </summary>
+        /// <param name="l">The language to add</param>
+        public void AddLanguage (Language l)
+        {
+            languages.Add(l);
+        }
+
+        /// <summary>
+        /// Return the language with the most words in the given text
+        /// </summary>
+        /// <param name="text">The text to analyze</param>
+        /// <returns>The language with the most words in the given text</returns>
+        public Language Analyze(string text)
+        {
+            string[] inputWords = text.Split(' ');
+            foreach (string word in inputWords)
+            {
+                ProcessWord(word);
+            }
+
+            return languages.MaxBy(x => x.Occurances);
+        }
+
+        /// <summary>
+        /// Updates the occurances for each language the word appears in
+        /// </summary>
+        /// <param name="word">The word to process</param>
+        private void ProcessWord(string word)
+        {
+            List<Language> languageList;
+            if ((languageList = words.FindWord(word)) != null)
+            {
+                languageList.ForEach(x => x.Occurances++);
+            }
+        }
+
+        public void Reset()
+        {
+            languages.ForEach(x => x.Occurances = 0);
+        }
+    }
+}
---
+++ langdetect-universal/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace langdetect_universal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        WordStorage wordStorage;
+        LangDetect langDetect;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size { Height = 480, Width = 375 };
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            wordStorage = new WordStorage();
+            langDetect = new LangDetect(wordStorage);
+        }
+
+        private async void detectLanguageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            langDetect.Reset();
+            Language lang = langDetect.Analyze(textBox.Text);
+            MessageDialog msg = new MessageDialog(lang.Name);
+            await msg.ShowAsync();
+        }
+
+        private async void selectStopFilesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker fp = new FolderPicker();
+            fp.FileTypeFilter.Add("*");
+            StorageFolder folder = await fp.PickSingleFolderAsync();
+
+            IReadOnlyList<StorageFile> files = await folder.GetFilesAsync();
+            foreach (StorageFile file in files)
+            {
+                Language lang = new Language(file.DisplayName); 
+                langDetect.AddLanguage(lang);
+
+                IList<string> words = await FileIO.ReadLinesAsync(file, UnicodeEncoding.Utf8);
+                foreach (string word in words)
+                {
+                    wordStorage.AddWord(word, lang);
+                }
+            }
+        }
+    }
+}
